24ea9352357e72b7d81d2a2cbbc2b3d9e8857971
bojosos <ivanov6@abv.bg>
2020-02-16 20:30:05 +0200

    side textures aren't flipped

--- Minecraft/src/game/block.cpp
+++ Minecraft/src/game/block.cpp
@@ -12,22 +12,22 @@ namespace Minecraft
 
 	void Block::GetLeftVertexData(vertex* res, uint8_t x, uint8_t y, uint8_t z, uint32_t& i) const
 	{
-		res[i++] = vertex(x, y, z, m_Data.negxU, m_Data.negxV, m_Tid);
-		res[i++] = vertex(x, y, z + 1, m_Data.negxU + 1, m_Data.negxV, m_Tid);
-		res[i++] = vertex(x, y + 1, z, m_Data.negxU, m_Data.negxV + 1, m_Tid);
-		res[i++] = vertex(x, y + 1, z, m_Data.negxU, m_Data.negxV + 1, m_Tid);
-		res[i++] = vertex(x, y, z + 1, m_Data.negxU + 1, m_Data.negxV, m_Tid);
-		res[i++] = vertex(x, y + 1, z + 1, m_Data.negxU + 1, m_Data.negxV + 1, m_Tid);
+		res[i++] = vertex(x, y, z, m_Data.negxU, m_Data.negxV + 1, m_Tid);
+		res[i++] = vertex(x, y, z + 1, m_Data.negxU + 1, m_Data.negxV + 1, m_Tid);
+		res[i++] = vertex(x, y + 1, z, m_Data.negxU, m_Data.negxV, m_Tid);
+		res[i++] = vertex(x, y + 1, z, m_Data.negxU, m_Data.negxV, m_Tid);
+		res[i++] = vertex(x, y, z + 1, m_Data.negxU + 1, m_Data.negxV + 1, m_Tid);
+		res[i++] = vertex(x, y + 1, z + 1, m_Data.negxU + 1, m_Data.negxV, m_Tid);
 	}
 
 	void Block::GetRightVertexData(vertex* res, uint8_t x, uint8_t y, uint8_t z, uint32_t& i) const
 	{
-		res[i++] = vertex(x + 1, y, z, m_Data.posxU, m_Data.posxV, m_Tid);
-		res[i++] = vertex(x + 1, y + 1, z, m_Data.posxU, m_Data.posxV + 1, m_Tid);
-		res[i++] = vertex(x + 1, y, z + 1, m_Data.posxU + 1, m_Data.posxV, m_Tid);
-		res[i++] = vertex(x + 1, y + 1, z, m_Data.posxU, m_Data.posxV + 1, m_Tid);
-		res[i++] = vertex(x + 1, y + 1, z + 1, m_Data.posxU + 1, m_Data.posxV + 1, m_Tid);
-		res[i++] = vertex(x + 1, y, z + 1, m_Data.posxU + 1, m_Data.posxV, m_Tid);
+		res[i++] = vertex(x + 1, y, z, m_Data.posxU, m_Data.posxV + 1, m_Tid);
+		res[i++] = vertex(x + 1, y + 1, z, m_Data.posxU, m_Data.posxV, m_Tid);
+		res[i++] = vertex(x + 1, y, z + 1, m_Data.posxU + 1, m_Data.posxV + 1, m_Tid);
+		res[i++] = vertex(x + 1, y + 1, z, m_Data.posxU, m_Data.posxV, m_Tid);
+		res[i++] = vertex(x + 1, y + 1, z + 1, m_Data.posxU + 1, m_Data.posxV, m_Tid);
+		res[i++] = vertex(x + 1, y, z + 1, m_Data.posxU + 1, m_Data.posxV + 1, m_Tid);
 	}
 
 	void Block::GetUpVertexData(vertex* res, uint8_t x, uint8_t y, uint8_t z, uint32_t& i) const
@@ -52,22 +52,22 @@ namespace Minecraft
 
 	void Block::GetBackVertexData(vertex* res, uint8_t x, uint8_t y, uint8_t z, uint32_t& i) const
 	{
-		res[i++] = vertex(x, y, z + 1, m_Data.poszU, m_Data.poszV, m_Tid);
-		res[i++] = vertex(x + 1, y, z + 1, m_Data.poszU + 1, m_Data.poszV, m_Tid);
-		res[i++] = vertex(x, y + 1, z + 1, m_Data.poszU, m_Data.poszV + 1, m_Tid);
-		res[i++] = vertex(x, y + 1, z + 1, m_Data.poszU, m_Data.poszV + 1, m_Tid);
-		res[i++] = vertex(x + 1, y, z + 1, m_Data.poszU + 1, m_Data.poszV, m_Tid);
-		res[i++] = vertex(x + 1, y + 1, z + 1, m_Data.poszU + 1, m_Data.poszV + 1, m_Tid);
+		res[i++] = vertex(x, y, z + 1, m_Data.poszU, m_Data.poszV + 1, m_Tid);
+		res[i++] = vertex(x + 1, y, z + 1, m_Data.poszU + 1, m_Data.poszV + 1, m_Tid);
+		res[i++] = vertex(x, y + 1, z + 1, m_Data.poszU, m_Data.poszV, m_Tid);
+		res[i++] = vertex(x, y + 1, z + 1, m_Data.poszU, m_Data.poszV, m_Tid);
+		res[i++] = vertex(x + 1, y, z + 1, m_Data.poszU + 1, m_Data.poszV + 1, m_Tid);
+		res[i++] = vertex(x + 1, y + 1, z + 1, m_Data.poszU + 1, m_Data.poszV, m_Tid);
 	}
 
 	void Block::GetFrontVertexData(vertex* res, uint8_t x, uint8_t y, uint8_t z, uint32_t& i) const
 	{
-		res[i++] = vertex(x, y, z, m_Data.negzU, m_Data.negzV, m_Tid);
-		res[i++] = vertex(x, y + 1, z, m_Data.negzU, m_Data.negzV + 1, m_Tid);
-		res[i++] = vertex(x + 1, y, z, m_Data.negzU + 1, m_Data.negzV, m_Tid);
-		res[i++] = vertex(x, y + 1, z, m_Data.negzU, m_Data.negzV + 1, m_Tid);
-		res[i++] = vertex(x + 1, y + 1, z, m_Data.negzU + 1, m_Data.negzV + 1, m_Tid);
-		res[i++] = vertex(x + 1, y, z, m_Data.negzU + 1, m_Data.negzV, m_Tid);
+		res[i++] = vertex(x, y, z, m_Data.negzU, m_Data.negzV + 1, m_Tid);
+		res[i++] = vertex(x, y + 1, z, m_Data.negzU, m_Data.negzV, m_Tid);
+		res[i++] = vertex(x + 1, y, z, m_Data.negzU + 1, m_Data.negzV + 1, m_Tid);
+		res[i++] = vertex(x, y + 1, z, m_Data.negzU, m_Data.negzV, m_Tid);
+		res[i++] = vertex(x + 1, y + 1, z, m_Data.negzU + 1, m_Data.negzV, m_Tid);
+		res[i++] = vertex(x + 1, y, z, m_Data.negzU + 1, m_Data.negzV + 1, m_Tid);
 	}
 
 }
